1534b16576eedb2dbe260c711a814432b7938ceb
hipotenusa <fonso.gs@gmail.com>
2017-12-19 23:15:30 +0100

    Update app.js

--- app.js
+++ app.js
@@ -9,7 +9,8 @@ app.get('/', function (req, res) {
     res.sendFile('/index.html');
 });
 
-var server = app.listen(8081, function () {
+var port = process.env.PORT || 8081;
+var server = app.listen(port, function () {
    var host = server.address().address
    var port = server.address().port
 
