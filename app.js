04585c13cdaf20aae3f39bcc05068379e748f8da
hipotenusa <fonso.gs@gmail.com>
2018-01-03 21:50:22 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 var mqtt = require('mqtt')
-var client = mqtt.connect('mqtt://aqainmze:g4aN3MgGSwN8@m23.cloudmqtt.com?clientId=abcde');
+var client = mqtt.connect('mqtt://m23.cloudmqtt.com');
 client.subscribe('esp/test');
 client.publish('esp/test', 'Hello mqtt');
 client.on('esp/test', function (topic, message) {
     console.log(message);
+    client.end();
 });
-client.end();
+
