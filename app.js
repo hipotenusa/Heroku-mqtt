834386cce90b265dcdbb171c4d8340aa601b6beb
hipotenusa <fonso.gs@gmail.com>
2018-01-03 21:25:53 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
 var express = require('express');
 var app = express();
-
 var server = require('http').Server(app);
+var mqtt = require('mqtt');
+var url = require('url');
+
+// Parse
+var mqtt_url = url.parse(process.env.CLOUDMQTT_URL || 'm14.cloudmqtt.com');
+var auth = (mqtt_url.auth || ':').split(':');
+
+// Create a client connection
+var client = mqtt.createClient(13433, "m14.cloudmqtt.com", {
+  username: "hvblvunn",
+  password: "2eHyr5j9cSHI"
+});
 
 // set the port of our application
 // process.env.PORT lets the port be set by Heroku
@@ -13,6 +24,23 @@ app.use(express.static(__dirname + '/public'));
 // set the home page route
 app.get('/', function (req, res) {
   res.sendfile(__dirname + '/index.html');
+});
+
+client.on('connect', function() { // When connected
+
+  // subscribe to a topic
+  client.subscribe('esp/test', function() {
+    // when a message arrives, do something with it
+    client.on('message', function(topic, message, packet) {
+      console.log("Received '" + message + "' on '" + topic + "'");
+    });
+  });
+
+  // publish a message to a topic
+  client.publish('esp/test', 'my message', function() {
+    console.log("Message is published");
+    client.end(); // Close the connection when published
+  });
 });
 
 server.listen(port, function() {
