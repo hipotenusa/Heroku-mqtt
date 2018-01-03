af57e852b304d8fe41a50f74863399dc7af75da5
hipotenusa <fonso.gs@gmail.com>
2018-01-03 21:44:14 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 var mqtt = require('mqtt')
-client = mqtt.connect('mqtt://aqainmze:g4aN3MgGSwN8@localhost?clientId=abcde');
+var client = mqtt.connect('mqtt://aqainmze:g4aN3MgGSwN8@localhost?clientId=abcde');
 client.subscribe('esp/test');
 client.publish('esp/test', 'Hello mqtt');
 client.on('esp/test', function (topic, message) {
