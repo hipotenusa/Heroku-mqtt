349caababc803b6abe9dc5b852a24f871bbcf8ca
hipotenusa <fonso.gs@gmail.com>
2018-01-03 21:07:13 +0100

    Update app.js

--- app.js
+++ app.js
@@ -2,11 +2,6 @@ var express = require('express');
 var app = express();
 
 var server = require('http').Server(app);
-var io =require('socket.io')(server);
-
-// set heartbeat parameters
-io.set('heartbeat timeout', 4000); 
-io.set('heartbeat interval', 2000);
 
 // set the port of our application
 // process.env.PORT lets the port be set by Heroku
@@ -18,26 +13,6 @@ app.use(express.static(__dirname + '/public'));
 // set the home page route
 app.get('/', function (req, res) {
   res.sendfile(__dirname + '/index.html');
-});
-
-io.sockets.on('connection', function(socket)
-{
-    console.log("Client has connected");
-
-    socket.on('disconnect', function(reason) {
-      console.log("Client has disconnected ->" + reason);
-    });
-
-    socket.on('theSwitch', function (data) {
-      console.log("Switch status = " + data.status);
-      io.sockets.emit('theSwitch', {status: data.status});
-    });
-
-    socket.on('theLed', function(data) {
-      console.log("Led status = " + data.status);
-      io.sockets.emit('theLed', {status: data.status});
-    });
-
 });
 
 server.listen(port, function() {
