8e3ac0fe739e8a7a0401e8ee60d32f690f2c6f7e
hipotenusa <fonso.gs@gmail.com>
2018-01-03 21:41:24 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,27 +1,8 @@
-var mqtt = require('mqtt'), url = require('url');
-// Parse
-var mqtt_url = url.parse(process.env.CLOUDMQTT_URL || 'mqtt://localhost:1883');
-var auth = (mqtt_url.auth || ':').split(':');
-
-// Create a client connection
-var client = mqtt.createClient(mqtt_url.port, mqtt_url.hostname, {
-  username: auth[0],
-  password: auth[1]
+var mqtt = require('mqtt')
+client = mqtt.connect('mqtt://aqainmze:g4aN3MgGSwN8@localhost?clientId=abcde');
+client.subscribe('esp/test');
+client.publish('esp/test', 'Hello mqtt');
+client.on('message', function (topic, message) {
+    console.log(message);
 });
-
-client.on('connect', function() { // When connected
-
-  // subscribe to a topic
-  client.subscribe('esp/test', function() {
-    // when a message arrives, do something with it
-    client.on('message', function(topic, message, packet) {
-      console.log("Received '" + message + "' on '" + topic + "'");
-    });
-  });
-
-  // publish a message to a topic
-  client.publish('hello/world', 'my message', function() {
-    console.log("Message is published");
-    client.end(); // Close the connection when published
-  });
-});
+client.end();
